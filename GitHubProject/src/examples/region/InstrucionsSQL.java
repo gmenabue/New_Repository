53416bb2b5801c2dc93f0768682cd9fc4ce1af22
gmenabue <giordanomenabue@gmail.com>
2015-06-03 21:32:23 +0200

    actualización

---
+++ GitHubProject/src/examples/region/InstrucionsSQL.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package examples.region;
+
+/**
+ * @author alumno
+ *
+ */
+public class InstrucionsSQL {
+	
+	public static final String consulta_region_all = "SELECT * FROM REGIONS";
+	
+	
+	public static final String consulta_region_id = "SELECT * FROM REGIONS WHERE REGION_ID = ?";
+
+}
---
+++ GitHubProject/src/examples/region/MainRegion.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package examples.region;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author alumno
+ *
+ */
+public class MainRegion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<RegionDTO> lista_regiones = new ArrayList<RegionDTO>();
+	lista_regiones = RegionDAO.recuperarPorID();
+	System.out.println(lista_regiones.toString());
+		// TODO Auto-generated method stub
+
+	}
+
+}
