7e4d71e9d74df8cf9d93ba6490c283c0f2576405
gmenabue <giordanomenabue@gmail.com>
2015-06-05 15:02:15 +0200

    Update

--- GitHubProject/src/evaluacion/primera/InstrucionsSQL.java
+++ GitHubProject/src/evaluacion/primera/InstrucionsSQL.java
@@ -12,6 +12,9 @@ public class InstrucionsSQL {
 	public static final String consulta_region_all = "SELECT * FROM REGIONS";
 	
 	
-	public static final String consulta_region_id = "SELECT * FROM REGIONS WHERE REGIONS_ID = ?";
+	public static final String consulta_region_id = "SELECT * FROM REGIONS WHERE REGION_ID = ?";
+	
+	
+	public static final String insertar_region = "INSERT INTO REGIONS (region_id, region_name) VALUES (?,?)";
 
 }
